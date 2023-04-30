f8391a2d6475e748f7ceaa6d51675b6d07ec7a8a
Devoodie <131494134+Devoodie@users.noreply.github.com>
2023-04-30 12:50:30 -0400

    tesr

--- src/cookdevmax.py
+++ src/cookdevmax.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Subject(BaseModel):
+    subject_id: int #primary
+    subject_name: str #unique
+
+class Items(BaseModel):
+    item_id: int #primary
+    subject: int #relational "subject_id"
+    publisher: str|None = None
+    media_type: str
+
+class page(BaseModel):
+    page:int #unique
+    book_id:int #relational "item_id"
+    content:str #dkhow to implement text
+    chapter:str
+    publisher:str|None = None #relational "publisher"
 
 @app.get("/hello")
 async def say_hello() -> str:
